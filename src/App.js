a92c9a3ea27c34ae04a8e04d256947662086378a
sabrine loussaief <loussaiefsabrine@gmail.comgit config --global user.email loussaiefsabrine@gmail.com>
2022-02-06 12:25:22 +0100

    JSX Checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './style.css';
+import img from './imgsrc.JPG';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+<h1 className="title red">Workshop JSX</h1>
+
+<br/>
+
+<img className="img2" src={img} alt="img" />
+
+
+<br/>
+
+<img className="img1" src="/imgpub.jpg" alt="img" />
+
+</div>
+
+<video className="vid" width="320" height="240" controls>
+
+<source src="myVideo.mp4" type="video/mp4" />
+
+</video>
     </div>
   );
 }
